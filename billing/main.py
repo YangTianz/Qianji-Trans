a1773dc4b8433d16d58f3bdd8d70900ed9a4b4a2
YangTianz <tingerytz@gmail.com>
2024-06-14 23:24:44 +0800

    改用 ytzlib 实现循环

--- billing/main.py
+++ billing/main.py
@@ -7,7 +7,10 @@ import re
 import subprocess
 import time
 
+from functools import partial
 from typing import Type  # noqa: F401
+
+from ytzlib.tick_helper import ticker
 
 from billing.bill_loader import AlipayBillLoader
 from billing.bill_loader import WeChatBillLoader
@@ -51,59 +54,51 @@ def load_rules_file(file_path: str) -> list:
     return unique_rules
 
 
-async def load_new_transactions_from_bill(
-    args: "BillingArgs", event: asyncio.Event
-) -> None:
+def load_from_raw_bill(args: "BillingArgs", event: asyncio.Event) -> None:
     """从微信支付宝账单数据读取新产生的交易，写入数据库"""
     all_transactions = load_transactions_from_db()
     dump_db(args.output_path)
-    interval = 3
     raw_data_path = os.path.join(args.work_dir, "!raw_bill")
     ensure_dir_exist(raw_data_path)
     dump_path = os.path.join(args.work_dir, "archived", "raw_bills")
     ensure_dir_exist(dump_path)
-    while True:
-        account_rules_list: list = load_rules_file(args.account_rules)
-        classify_rules_list: list = load_rules_file(args.classify_rules)
-        loaders = [
-            WeChatBillLoader,
-            AlipayBillLoader,
-        ]  # type: list[Type[Loader]]
-        new_transactions: dict[str, QianjiTransaction] = {}
-        for loader_cls in loaders:
-            loader: Loader = loader_cls(
-                account_rules_list, classify_rules_list
-            )
 
-            def file_name_validator(
-                file_name: str, regex_pattern: str = loader.file_regex
-            ) -> bool:
-                regex = re.compile(regex_pattern)
-                return regex.match(file_name) is not None
+    account_rules_list: list = load_rules_file(args.account_rules)
+    classify_rules_list: list = load_rules_file(args.classify_rules)
+    loaders = [
+        WeChatBillLoader,
+        AlipayBillLoader,
+    ]  # type: list[Type[Loader]]
+    new_transactions: dict[str, QianjiTransaction] = {}
+    for loader_cls in loaders:
+        loader: Loader = loader_cls(account_rules_list, classify_rules_list)
 
-            file_contents = scan_and_move(
-                raw_data_path, dump_path, file_name_validator
+        def file_name_validator(
+            file_name: str, regex_pattern: str = loader.file_regex
+        ) -> bool:
+            regex = re.compile(regex_pattern)
+            return regex.match(file_name) is not None
+
+        file_contents = scan_and_move(
+            raw_data_path, dump_path, file_name_validator
+        )
+        for csv_file_contents in file_contents.values():
+            transactions = loader.parse_file_content(
+                csv_file_contents.decode(loader.encoding)
             )
-            for csv_file_contents in file_contents.values():
-                transactions = loader.parse_file_content(
-                    csv_file_contents.decode(loader.encoding)
-                )
-                for transaction in transactions:
-                    tid = transaction.id
-                    if transaction.is_valid() and tid not in all_transactions:
-                        new_transactions[tid] = transaction
-        if new_transactions:
-            logger.show(
-                "[load transactions from wechat and alipay][new count=%s]"
-                % len(new_transactions),
-            )
-            event.set()
-            insert_transactions(
-                list(new_transactions.values()), TranStatus.Raw
-            )
-            dump_db(args.output_path)
-            all_transactions.update(new_transactions)
-        await asyncio.sleep(interval)
+            for transaction in transactions:
+                tid = transaction.id
+                if transaction.is_valid() and tid not in all_transactions:
+                    new_transactions[tid] = transaction
+    if new_transactions:
+        logger.show(
+            "[load transactions from wechat and alipay][new count=%s]"
+            % len(new_transactions),
+        )
+        event.set()
+        insert_transactions(list(new_transactions.values()), TranStatus.Raw)
+        dump_db(args.output_path)
+        all_transactions.update(new_transactions)
 
 
 async def output_confirmed_data(
@@ -111,28 +106,25 @@ async def output_confirmed_data(
 ) -> None:
     """将未分类交易输出到文件，等待用户确认"""
     dump_path = os.path.join(args.work_dir, "unconfirmed.csv")
-    while True:
-        await event.wait()
-        unconfirmed_t = load_transactions_from_db(TranStatus.Raw)
-        if unconfirmed_t:
-            output = [t.dump() for t in unconfirmed_t.values()]
-            output.insert(0, HEADER)
-            with open(dump_path, "wb") as f:
-                b = bytes("\n".join(output), encoding="utf-8")
-                f.write(codecs.BOM_UTF8 + b)
-                logger.show(
-                    "[write unconfirmed transactions][count=%s]"
-                    % len(unconfirmed_t)
-                )
-        event.clear()
-        await asyncio.sleep(1)
+    await event.wait()
+    unconfirmed_t = load_transactions_from_db(TranStatus.Raw)
+    if unconfirmed_t:
+        output = [t.dump() for t in unconfirmed_t.values()]
+        output.insert(0, HEADER)
+        with open(dump_path, "wb") as f:
+            b = bytes("\n".join(output), encoding="utf-8")
+            f.write(codecs.BOM_UTF8 + b)
+            logger.show(
+                "[write unconfirmed transactions][count=%s]"
+                % len(unconfirmed_t)
+            )
+    event.clear()
 
 
 async def handle_confirmed_data(
     args: "BillingArgs", queue: asyncio.Queue
 ) -> None:
     """将已确认的数据入库"""
-    interval = 3
     input_path = os.path.join(args.work_dir, "confirmed")
     ensure_dir_exist(input_path)
     dump_path = os.path.join(args.work_dir, "archived", "confirmed")
@@ -141,43 +133,36 @@ async def handle_confirmed_data(
     def file_name_validator(file_name: str) -> bool:
         return file_name == "unconfirmed.csv"
 
-    while True:
-        postfix = "-" + str(int(time.time()))
-        contents = scan_and_move(
-            input_path, dump_path, file_name_validator, postfix=postfix
+    postfix = "-" + str(int(time.time()))
+    contents = scan_and_move(
+        input_path, dump_path, file_name_validator, postfix=postfix
+    )
+    for _, content in contents.items():
+        confirmed = QianjiTransaction.load_from_file_content(
+            content.decode("utf-8")
         )
-        for _, content in contents.items():
-            confirmed = QianjiTransaction.load_from_file_content(
-                content.decode("utf-8")
+        if confirmed:
+            insert_transactions(
+                list(confirmed.values()), TranStatus.Classified
             )
-            if confirmed:
-                insert_transactions(
-                    list(confirmed.values()), TranStatus.Classified
-                )
-                dump_db(args.output_path)
-                logger.show(
-                    "[transactions confirmed][count=%s]" % len(confirmed)
-                )
-                for t in confirmed.values():
-                    await queue.put(t)
-        await asyncio.sleep(interval)
+            dump_db(args.output_path)
+            logger.show("[transactions confirmed][count=%s]" % len(confirmed))
+            for t in confirmed.values():
+                await queue.put(t)
 
 
 async def finial_adb_output(args: "BillingArgs", queue: asyncio.Queue) -> None:
-    adb_interval = 4
-    while True:
-        transaction: QianjiTransaction = await queue.get()
-        adb_cmd = transaction.dump_to_adb_command()
-        insert_transactions(
-            [
-                transaction,
-            ],
-            TranStatus.Written,
-        )
-        dump_db(args.output_path)
-        logger.debug(adb_cmd)
-        subprocess.run(adb_cmd, shell=True)
-        await asyncio.sleep(adb_interval)
+    transaction: QianjiTransaction = await queue.get()
+    adb_cmd = transaction.dump_to_adb_command()
+    insert_transactions(
+        [
+            transaction,
+        ],
+        TranStatus.Written,
+    )
+    dump_db(args.output_path)
+    logger.debug(adb_cmd)
+    subprocess.run(adb_cmd, shell=True)
 
 
 async def main() -> None:
@@ -186,21 +171,19 @@ async def main() -> None:
     logger.init(os.path.join(args.work_dir, "log.txt"))
     check_and_create_database()
     unconfirmed_transaction_event = asyncio.Event()
-    coroutines = []
-    coroutines.append(
-        load_new_transactions_from_bill(args, unconfirmed_transaction_event)
-    )
-    coroutines.append(
-        output_confirmed_data(args, unconfirmed_transaction_event)
-    )
+    func = partial(load_from_raw_bill, args, unconfirmed_transaction_event)
+    ticker.repeat_call(func, 3.0)
+    func = partial(output_confirmed_data, args, unconfirmed_transaction_event)
+    ticker.repeat_call(func, 1.0)
     uninsert_transaction_list: asyncio.Queue = asyncio.Queue()
-    coroutines.append(handle_confirmed_data(args, uninsert_transaction_list))
-    coroutines.append(finial_adb_output(args, uninsert_transaction_list))
-    await asyncio.gather(*coroutines)
+    func = partial(handle_confirmed_data, args, uninsert_transaction_list)
+    ticker.repeat_call(func, 3.0)
+    func = partial(finial_adb_output, args, uninsert_transaction_list)
+    ticker.repeat_call(func, 4.0)
+    await asyncio.gather(ticker.start())
 
 
 class BillingArgs(argparse.Namespace):
-
     def __init__(self) -> None:
         self.account_rules: str = ""
         self.classify_rules: str = ""
